88a11a4ba1523f9530c1fd28d05dc293cc35f0f1
Chan Pyae <114207295+chanpyaephyoaung@users.noreply.github.com>
2022-11-27 21:34:30 +0630

    Initialize HTML base layout

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const data = [
+  {
+    id: 1,
+    location: "Lotte Hotel, Yangon",
+    coords: ["lat", "lng"],
+    activities: ["Pooping", "Screaming", "Running"],
+    checkinTime: "12:00am",
+    checkoutTime: "7:00pm",
+  },
+];
